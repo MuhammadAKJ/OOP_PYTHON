cfe7386f397b21e0bf1d664d4670bcad923429ca
MuhammadAKJ <Muhammadakj11@gmail.com>
2024-12-05 02:38:23 +0100

    Cpp Vector
    added vector and make tuple to return 3d cordinates location of UAV

--- drones/drone.cpp
+++ drones/drone.cpp
@@ -2,17 +2,24 @@
 #include <iostream>
 #include <cassert>
 #include <tuple>
+#include <vector>
 
 const int MAX_X = 1366;
 const int MAX_Y = 768;
 const int MAX_Z = 100;
 
-class Drone {
+class AbstractClass{
+    virtual void collision() = 0;
+};
+
+class Drone: AbstractClass {
 private:
     std::string name;
     int x_cor, y_cor, z_cor;
 
 public:
+    //Public variables
+    std::vector<std::string> *drone_location;
     // Constructor
     Drone(std::string aName, int aX, int aY, int aZ) {
         assert(aX > 0 && aX < MAX_X && "x must be positive and in range");
@@ -22,8 +29,15 @@ public:
         x_cor = aX;
         y_cor = aY;
         z_cor = aZ;
+        
     }
 
+    //Collision contract implementation
+    void collision(){
+
+    }
+
+    //Get coordinates and return tuples
     std::tuple<int, int, int> getCordinates() {
         return std::make_tuple(x_cor, y_cor, z_cor);
     }
@@ -38,10 +52,12 @@ public:
         z_cor = aZ;
         std::cout << "Coordinates updated successfully" << std::endl;
     }
+
+    
 };
 
 int main() {
-    Drone drone("drone1", 23, 33, 32);
+    Drone drone("drone1", 239, 33, 32);
     std::cout << "Drone Name: " << drone.getName() << std::endl;
 
     // Print coordinates
